bf8ee14bb596096f740c7a5fa7e00eddd63863c6
xinghai-9 <763971579@qq.com>
2020-09-27 00:01:23 +0800

    '引入ElementUI'

---
+++ src/components/content/topBar/TopBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="top-bar">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TopBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <top-bar/>
+  </div>
+
+</template>
+
+<script>
+  import TopBar from "components/content/topBar/TopBar";
+
+  export default {
+    name: "Home",
+    components: {
+      TopBar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
